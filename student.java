19d8ca2058641328aacbfc3dc0445de2542d6a6f
Neha Chakravarthy Madhavamahanthi <madhavamahanthinehachakravarth@gmail.com>
2026-08-09 22:32:11 +0530

    initial commit

---
+++ student.java
@@ -0,0 +1,7 @@
+public class student {
+    public static void main(String[] args) {
+        System.out.println("Name:Neha");
+        System.out.println("Roll No:A25126552229");
+        System.out.println("Welcome to core Java");
+    }
+}
---
+++ welcome.java
@@ -0,0 +1,6 @@
+public class welcome {
+    public static void main(String[] args){
+        System.out.println("Name:Neha");
+        System.out.println("Welcome to Java");
+    }
+}
