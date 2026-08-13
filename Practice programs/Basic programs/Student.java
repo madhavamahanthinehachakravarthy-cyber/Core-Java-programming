7bb414553f145788d967c031778e65b0ddb06691
madhavamahanthinehachakravarthy-cyber <madhavamahanthinehachakravarth@gmail.com>
2026-08-13 21:50:33 +0530

    Add files via upload

---
+++ Practice programs/Basic programs/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+public static void main (String[] args) { System.out.println("Name:Neha"); 
+System.out.println(
+"Roll No:A25126552229"); System.out.println(
+
+"Welcome to core Java");
+
+}
+
+}
