030c7624d57dd15866f585c0b0a343b29e6369e8
madhavamahanthinehachakravarthy-cyber <madhavamahanthinehachakravarth@gmail.com>
2026-08-13 13:28:25 +0530

    Delete student.java

--- student.java
+++
@@ -1,7 +0,0 @@
-public class student {
-    public static void main(String[] args) {
-        System.out.println("Name:Neha");
-        System.out.println("Roll No:A25126552229");
-        System.out.println("Welcome to core Java");
-    }
-}
