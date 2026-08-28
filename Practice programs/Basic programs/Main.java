174e87f9843f1d424f2c2e0fe86f8e95242d4d88
madhavamahanthinehachakravarthy-cyber <madhavamahanthinehachakravarth@gmail.com>
2026-08-28 11:23:23 +0530

    Add files via upload

---
+++ Practice programs/Basic programs/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    static void greetUser(String name, int age) {
+        System.out.println("Hello " + name + "! You are " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+       
+        greetUser("Neha", 25); 
+        greetUser("Naidu", 32);
+    }
+}
